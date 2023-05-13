b0dad3868d7e7da8f980657b4691f92e92247028
puranchandralohar <clpuran95@gmail.com>
2023-05-13 16:14:59 +0530

    camera icon

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const video = document.getElementById('stream');
 const button = document.querySelector(".button");
 let canvas = document.querySelector("#canvas");
 let clickSound = document.getElementById('click_sound');
+let startCam = document.querySelector('.start_cam')
+let homeBtn = document.querySelector('.home');
 
 
-navigator.mediaDevices.getUserMedia({ video: true })
+startCam.addEventListener('click',()=>{
+  navigator.mediaDevices.getUserMedia({ video: true })
   .then(stream => {
     video.srcObject = stream;
     video.play();
@@ -12,8 +15,6 @@ navigator.mediaDevices.getUserMedia({ video: true })
   .catch(error => {
     console.error('Camera Loading Failed', error);
   });
-
-
 
   button.addEventListener('click', function() {
     button.style.background = "#1b1b1b";
@@ -24,4 +25,10 @@ navigator.mediaDevices.getUserMedia({ video: true })
     canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
     let image_data_url = canvas.toDataURL('image/jpeg');
     console.log(image_data_url);
+  
+});
+})
+
+homeBtn.addEventListener('click', ()=>{
+
 });
